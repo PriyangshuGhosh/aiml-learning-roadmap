110324877ee2668874aabd062071b0ecdc744b1d
PriyangshuGhosh <priyangshu0906@gmail.com>
2025-12-16 23:25:01 +0530

    3d Plotting using mpl_toolkits.mplot3d -> Axes3D

--- src/RandomModuleDataGeneratoin.py
+++ src/RandomModuleDataGeneratoin.py
@@ -19,6 +19,9 @@ plt.savefig("Scatter plot ")
 ##############################################
 # generating random data with probability distribution
 ##############################################
+"""
+This is not right as it plots in 2 dimensions only 
+
 
 Y=np.random.choice([0,1,2],size = 1000, p=[0.1,0.2,0.7])
 print(np.sum(Y==0))
@@ -30,6 +33,24 @@ fig,ax = plt.subplots()
 ax.scatter(X[:,0],X[:,1],X[:,2],c=Y)
 plt.xlabel("X1")
 plt.ylabel("X2")
-plt.zlabel("X3")
 plt.title("Random choice generator")
 plt.savefig("Random generator scatter plot")
+"""
+
+##############################################
+# Correct 3d plotting with probability distribution
+##############################################
+
+from mpl_toolkits.mplot3d import Axes3D
+Y=np.random.choice([1,2,3],size=1000 , p = [0.5,0.4,0.1])
+X=np.random.randn(1000,3)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(X[:,0],X[:,1],X[:,2],c=Y)
+
+ax.set_xlabel("X1")
+ax.set_ylabel("X2")
+ax.set_zlabel("X3")
+ax.set_title("3D plotting using mpl_toolkits.mplot3d -> Axes3D")
+plt.savefig("3D Plotting")
